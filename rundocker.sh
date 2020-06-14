d4d12f1f55132880e618712b0fff01f37f7371c0
Dan DuLeone <dan@duleone.com>
2020-06-14 10:29:48 -0400

    Removing the data from the repo, and using port 8080 on host

--- rundocker.sh
+++ rundocker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t penn-interactive-challenge .
-docker run -it --rm -p 80:80 --name penn-interactive-challenge penn-interactive-challenge
+docker run -it --rm -p 8080:80 --name penn-interactive-challenge penn-interactive-challenge
